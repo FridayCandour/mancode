3d670acc403fa80b501bc2249874af90bf01d346
fridaycandour <fridaymichaels662@gmail.com>
2024-03-03 04:15:20 +0100

    Auto saved workflow

--- app/src/pages/home/hero.js
+++ app/src/pages/home/hero.js
@@ -1,10 +1,12 @@
-import { div, h1, h2 } from "cradova";
+import { div, h1, h2, p } from "cradova";
 
 export const homeHero = div(
   { className: "mc-hero-bg" },
   div(
     { className: "flex-y" },
-    h1("Modern analytics"),
-    h1("for the modern world")
+    h1("Modern analytics", { className: "mc-hero-h1" }),
+    h1("for the modern world", { className: "mc-hero-h1" }),
+    p(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.`),
+    p(`Suspendisse varius enim in eros elementum tristique.`)
   )
 );
