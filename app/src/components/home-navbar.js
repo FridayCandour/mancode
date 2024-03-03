433af8c73e14b631283fb069c3dbd04bf0f4ad60
fridaycandour <fridaymichaels662@gmail.com>
2024-03-03 01:54:17 +0100

    Auto saved workflow

--- app/src/components/home-navbar.js
+++ app/src/components/home-navbar.js
@@ -1,11 +1,18 @@
-import { div, img, nav, span } from "cradova";
+import { button, div, img, nav, span } from "cradova";
 
 export const HomeNav = nav(
   { className: "mc-h-nav" },
   div(
     { className: "flex gap-4" },
     img({ src: "transparent-logo-shot.webp", className: "top-logo" }),
-    span("Vaultflow", { className: "t-ececec" })
+    span("Mancode", { className: "t-ececec" })
   ),
-  div(span)
+  div(
+    { className: "flex gap-24 all-t-ececec m-y-auto all-t-center" },
+    span("Features"),
+    span("Pricing"),
+    span("About us"),
+    button("Download the app", { className: "f-t-0B081C" }),
+    button("Talk to an expert")
+  )
 );
